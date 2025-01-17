658d7015ca6165b05a70dc81d2159c4e0f399bea
Ki Yung Ahn <kyagrd@gmail.com>
2025-01-17 12:57:07 +0900

    Update TutElpiScribble.v

--- TutElpiScribble.v
+++ TutElpiScribble.v
@@ -3,6 +3,9 @@ From elpi Require Import elpi.
   should start with above line
   before typing in examples from
   https://lpcic.github.io/coq-elpi/tutorial_elpi_lang.html
+
+  Syntax highlithing for Coq-Elip script (*.v extension files) may not work right after the codespace setup.
+  Just refresh the browser and syntax highlithing will start to work in the codespace web-vscode.
 *)
 
 Elpi Program tutorial lp:{{
